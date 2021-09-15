00e62059b81429990610f1565ce2f60c2cfd54ea
suyeon-hong <bellahong95@gmail.com>
2021-09-15 19:48:48 +0900

    community 페이지에 탭메뉴 생성 서브페이지 메뉴바 수정

--- js/main.js
+++ js/main.js
@@ -5,3 +5,24 @@ btnMo.addEventListener("click", ()=>{
     btnMo.classList.toggle("on");
     gnbMo.classList.toggle("on");
 });
+
+const btns = document.querySelectorAll(".community dl dt a");
+const boxs = document.querySelectorAll(".community dl dd");
+
+for (let i=0; i<btns.length; i++){
+    btns[i].addEventListener("click", e=>{
+        e.preventDefault();
+
+        let isOn = e.currentTarget.classList.contains("on");
+        if (isOn) return;
+
+        for (let el of btns){
+            el.classList.remove("on");
+        }
+        e.currentTarget.classList.add("on");
+        for (let el of boxs){
+            el.style.display = "none";
+        }
+        boxs[i].style.display = "block";
+    })
+}
