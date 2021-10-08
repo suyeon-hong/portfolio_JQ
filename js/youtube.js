99bce96c473ee267e8bcde58615a013b5f572f47
suyeon-hong <bellahong95@gmail.com>
2021-10-08 19:50:07 +0900

    youtube페이지 디자인 완성

--- js/youtube.js
+++ js/youtube.js
@@ -9,9 +9,16 @@ $.ajax({
     }
 }).success(function(data){
     let items = data.items;
-    console.log(items);
 
     $(items).each(function(index, data){
+        let txt = data.snippet.description;
+        let len = txt.length;
+        let date = data.snippet.publishedAt.split("T");
+
+        if (len > 50){
+            txt = txt.substr(0, 50)+ "..";
+        }
+
         $("#vidGallery").append(
             $("<article>").append(
                 $("<a>").attr({href : data.snippet.resourceId.videoId}).append(
@@ -19,8 +26,8 @@ $.ajax({
                 ),
                 $("<div class='con'>").append(
                     $("<h2>").text(data.snippet.title),
-                    $("<p>").text(data.snippet.description),
-                    $("<span>").text(data.snippet.publishedAt)
+                    $("<p>").text(txt),
+                    $("<span>").text(date[0])
                 )
             )
         )
