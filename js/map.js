89c16c176e0304f447a5e548edf22b1399203660
suyeon-hong <bellahong95@gmail.com>
2021-10-13 00:12:21 +0900

    location페이지 디자인 적용

--- js/map.js
+++ js/map.js
@@ -1,50 +1,62 @@
 const mapContainer = document.getElementById('map'),
     mapOption = { 
-        center: new kakao.maps.LatLng(37.51270644775243, 127.05879394664093),
+        center: new kakao.maps.LatLng(37.545566976145004, 127.22368010224646),
         level: 3
     };
 const map = new kakao.maps.Map(mapContainer, mapOption);
-const t_on = document.querySelectorAll(".location .traffic li")[0];
-const t_off = document.querySelectorAll(".location .traffic li")[1];
+const tBtn = document.querySelector(".location .traffic");
 
 // 교통정보 보기 버튼
-t_on.addEventListener("click", e=>{
+map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+tBtn.addEventListener("click", (e)=>{
     e.preventDefault();
 
-    t_off.classList.remove("on");
-    t_on.classList.add("on");
-    map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-})
-
-t_off.addEventListener("click", e=>{
-    e.preventDefault();
-    
-    t_on.classList.remove("on");
-    t_off.classList.add("on");
-    map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);  
+    let isOn = tBtn.classList.contains("on");
+    if(isOn){
+        tBtn.classList.remove("on");
+        map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+    }else{
+        tBtn.classList.toggle("on");
+        map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+    }
 })
 
 // 마커 표시
+const map_btns = document.querySelectorAll(".location .address article");
 const positions = [
     {
-        title: '코엑스', 
-        latlng: new kakao.maps.LatLng(37.51270644775243, 127.05879394664093)
+        title: '스타필드 하남', 
+        latlng: new kakao.maps.LatLng(37.545566976145004, 127.22368010224646),
+        button: map_btns[0]
     },
     {
-        title: '메가박스', 
-        latlng: new kakao.maps.LatLng(37.51306714609753, 127.05819475019142)
+        title: '스타필드 코엑스몰', 
+        latlng: new kakao.maps.LatLng(37.511354442426004, 127.05978822539129),
+        button: map_btns[1]
     },
     {
-        title: '스타필드', 
-        latlng: new kakao.maps.LatLng(37.51130939795544, 127.05977687879485)
+        title: '스타필드 시티 위례', 
+        latlng: new kakao.maps.LatLng(37.48010154291463, 127.14840202223859),
+        button: map_btns[2]
     }
 ];
+const imageSrc = "img/marker_icon.png";
 
 for (let i = 0; i < positions.length; i ++) {
+    let imageSize = new kakao.maps.Size(40, 40);
+    let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
     let marker = new kakao.maps.Marker({
         map: map,
         position: positions[i].latlng,
-        title : positions[i].title
+        title : positions[i].title,
+        image : markerImage
+    });
+    map_btns[i].addEventListener("click", ()=>{
+        for (let k = 0; k < map_btns.length; k++){
+            map_btns[k].classList.remove("on");
+        }
+        positions[i].button.classList.add("on");
+        panTo(positions[i].latlng);
     });
 }
 
@@ -58,6 +70,13 @@ map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 window.addEventListener("resize", ()=>{
     for (let i = 0; i < positions.length; i ++) {
-        map.panTo(new kakao.maps.LatLng(37.51270644775243, 127.05879394664093));
+        let target = document.querySelector(".location .address article.on");
+        let index = target.getAttribute("data-index");
+        panTo(positions[index].latlng);
     }
 })
+
+// 지도위치 이동
+function panTo(target) {
+    map.panTo(target);            
+}  
