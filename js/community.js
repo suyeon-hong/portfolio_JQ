b885b1a8e02f0be001d586526f3d118593ca99c3
suyeon-hong <bellahong95@gmail.com>
2021-10-14 16:49:33 +0900

    community페이지 tabs버튼 on기능 적용

--- js/community.js
+++ js/community.js
@@ -16,6 +16,16 @@ $tabBtns.on("click focus", function(e){
     $(target).show();
 });
 
+// community tabs 버튼
+const $tabs = $(".community .tabs li a");
+
+$tabs.on("click", function(e){
+    e.preventDefault();
+
+    $tabs.removeClass("on");
+    $(this).addClass("on");
+});
+
 // community QnAbox 토글 설정
 const $qbox = $("#faq .qna dt");
 const $abox = $("#faq .qna dd");
