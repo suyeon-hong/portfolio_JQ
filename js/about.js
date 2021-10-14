abacc3460e63262cd71d4e9cb09a4ddae00c7565
suyeon-hong <bellahong95@gmail.com>
2021-10-14 21:20:41 +0900

    about페이지 orgchart 추가

--- js/about.js
+++ js/about.js
@@ -13,3 +13,19 @@ $btns_service.on("click", function(e){
     $(".services img").hide();
     $(id).fadeIn(speed/2);
 });
+
+// team 더보기버튼
+const $teamBtn = $(".about .team .btnMore");
+const $orgClose = $(".about .orgchart .close");
+
+$teamBtn.on("click", function(e){
+    e.preventDefault();
+
+    $(".orgchart").addClass("on");
+});
+
+$orgClose.on("click", function(e){
+    e.preventDefault();
+
+    $(".orgchart").removeClass("on");
+});
