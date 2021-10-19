1e233cdb20698cca5a756a8a52be37f9f2442382
suyeon-hong <bellahong95@gmail.com>
2021-10-19 19:13:24 +0900

    gnb 2depth패널 추가

--- js/main.js
+++ js/main.js
@@ -21,6 +21,25 @@ for(let i=0; i<gnbMo_tabs.length; i++){
     });
 }
 
+// gnb 2depth menu
+$("#gnb>li").on("mouseenter", function(){
+    $(this).find(".sub").show();
+});
+
+$("#gnb>li").on("mouseleave", function(){
+    $(this).find(".sub").hide();
+});
+
+$("#gnb>li").each(function(index){
+    $("#gnb>li").eq(index).find("a").on("focusin", function(){
+        $("#gnb>li").eq(index).find(".sub").show();
+    })
+    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
+        $("#gnb>li").eq(index).find(".sub").hide();
+    })
+});
+
+
 // index visual slide
     const $visual = $("#visual");
     const $wrap = $visual.find(".wrap");
