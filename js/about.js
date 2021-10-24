84efca36568da909b82522141fd98d933e18cc23
suyeon-hong <bellahong95@gmail.com>
2021-10-24 16:55:18 +0900

    about project more페이지 추가

--- js/about.js
+++ js/about.js
@@ -14,6 +14,34 @@ $btns_service.on("click", function(e){
     $(id).fadeIn(500);
 });
 
+// project 더보기 버튼
+const $projectBtn = $(".about .projects .btnMore");
+const $moreClose = $(".about .projects .btnClose");
+
+$projectBtn.on("click", function(e){
+    e.preventDefault();
+
+    $(".about .projects .swiper").addClass("on");
+});
+$moreClose.on("click", function(e){
+    e.preventDefault();
+
+    $(".about .projects .swiper").removeClass("on");
+});
+
+let swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
 // team 더보기버튼
 const $teamBtn = $(".about .team .btnMore");
 const $orgClose = $(".about .team .orgchart .close");
