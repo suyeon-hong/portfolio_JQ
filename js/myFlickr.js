45c4342a70d998e14f95db60b6a99415f81c21dc
suyeon-hong <bellahong95@gmail.com>
2021-10-26 16:40:57 +0900

    gallery페이지 loading이미지 추가/디자인수정

--- js/myFlickr.js
+++ js/myFlickr.js
@@ -4,17 +4,20 @@ https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
 
 21be590b77fb11bd12a7266f99a2f2d8
 */
+let tag = "castle";
+let totalImg = 18;
+let imgNum = 0;
 
 $.ajax({
     url: "https://www.flickr.com/services/rest/?method=flickr.photos.search",
     datatype: "json",
     data: {
         api_key: "21be590b77fb11bd12a7266f99a2f2d8",
-        per_page: 20,
+        per_page: totalImg,
         format: "json",
         nojsoncallback: 1,
         privacy_filter: 5,
-        tags: "castle"
+        tags: tag,
     }
 }).success(function(data){
     let items = data.photos.photo;
@@ -25,25 +28,35 @@ $.ajax({
         if(!data.title){
             title = "No description in this photo"
         }
-        $(".gallery ul").append(
+        $(".gallery .photos").append(
             $("<li class='item'>").append(
-                $("<div class='pic'>").append(
-                    $("<a>").attr({
-                        href: "https://live.staticflickr.com/"+ data.server +"/"+ data.id +"_"+ data.secret +"_b.jpg",
-                        title: "새창열림"
-                    }).append(
-                        $("<img>").attr({
-                            src: "https://live.staticflickr.com/"+ data.server +"/"+ data.id +"_"+ data.secret +"_m.jpg"
-                        })
-                    )
-            ),
+                $("<a>").attr({
+                    href: "https://live.staticflickr.com/"+ data.server +"/"+ data.id +"_"+ data.secret +"_b.jpg",
+                    title: "새창열림"
+                }).append(
+                    $("<img>").attr({
+                        src: "https://live.staticflickr.com/"+ data.server +"/"+ data.id +"_"+ data.secret +"_m.jpg"
+                    })
+                ),
                 $("<h2>").text(title),
-                $("<img class='profile'>").attr({
-                    src: "http://farm"+ data.farm +".staticflickr.com/"+ data.server +"/buddyicons/"+ data.owner +".jpg"
-                })
+                $("<h3>").text(tag).append(
+                    $("<span>").text(data.owner)
+                )
             )
         )
     });
+
+    $(".photos img").each(function(index, data){
+        data.onload = function(){
+            imgNum++;
+
+            if (imgNum == totalImg){
+                $(".gallery .inner .loading").addClass("off");
+            }
+            $(".gallery .inner .photos").addClass("on");
+        }
+    });
+
 }).error(function(err){
     console.error("데이터를 불러오지 못했습니다.");
 })
