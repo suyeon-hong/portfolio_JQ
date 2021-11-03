8e16e1648675040bd57dd18d8dcc8c094066a08b
suyeon-hong <bellahong95@gmail.com>
2021-11-03 19:44:30 +0900

    메인 비주얼 모션 추가

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 
 let isActive = false;
 let num = 1;
+letter($("#visual .txt h1"), 0.1);
 
 $(".next").on("click", function(e){
     e.preventDefault();
@@ -14,16 +15,18 @@ $(".next").on("click", function(e){
         isActive = true;
         (num < 3) ? num++ : num=1;
         $(this).addClass("on");
+        $("#visual .txt h1 span").css({transform: "translateY(-10px)", color: "#000"});
         $("#visual .txt p").addClass("on");
         $(".num h1").css({transitionDelay: "0s"});
         $(".num").css({transition: "0.4s 0s"}).removeClass("on");
-        $("#visual .util li").each(function(index, data){
-            $(this).css({transition: "0.4s "+ index*0.2 +"s"});
+        $("#visual .util li").each(function(index,_){
+            $(this).css({transition: "0.4s "+ index * 0.2 +"s"});
         }).addClass("on");
         $("#visual .frame .wrap").css({filter: "grayscale(70%) blur(2px)"});
         $("#visual .frame .wrap").animate({marginLeft: "-100%"}, 700, function(){
             $(".next").removeClass("on");
             $("#visual .txt p").removeClass("on");
+            $("#visual .txt h1 span").css({transform: "translateY(0)", color: "#333"});
             $(this).css({marginLeft: 0, filter: "grayscale(40%)"})
             $("#visual .frame .wrap").find("img").first().appendTo("#visual .frame .wrap");
             $(".num h1").text("0"+ num);
@@ -46,7 +49,10 @@ const $tit1 = $("#intro h1");
 const $tit2 = $("#intro h2");
 
 letter($tit1, 0.1);
+letterBox($tit1);
 letter($tit2, 0.2);
+letterBox($tit2);
+
 
 function letter(item, delay){
     let txt = item.text();
@@ -56,10 +62,13 @@ function letter(item, delay){
 
     for(let el of txt){
         item.append(
-            $("<span>").text(el).css({transitionDelay: num * delay + 0.4 +"s"})
+            $("<span>").text(el).css({transitionDelay: num * delay +"s"})
         );
         num++;
     }
+}
+
+function letterBox(item){
     let bg = item.find("span").css("color");
     item.append(
         $("<p>").css({background: bg})
