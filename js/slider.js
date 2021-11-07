32bcc12c3aae072675e7d5bfdc0ae4135c391511
suyeon-hong <bellahong95@gmail.com>
2021-11-07 18:43:35 +0900

    slider script파일 분리

---
+++ js/slider.js
@@ -0,0 +1,93 @@
+let enableClick = true;
+
+
+// box1 slide
+$(".box1 .btns li").on("click", function(){
+
+    if(enableClick){
+        enableClick =false;
+        let index = $(this).index();
+
+        $(".box1 .btns li").removeClass("on");
+        $(this).addClass("on");
+        $(".slide-wrapper").animate({left: -(100 * index) +"%"}, speed/2, function(){
+            enableClick = true;
+        });
+    }
+});
+
+//slidebox slide
+$(".mento .slider-wrapper .slide").last().prependTo(".mento .slider-wrapper");
+
+let timer = setInterval(function(){
+    movingRight(".mento .slider-wrapper", ".slider", -100, -50);
+}, speed*2);
+
+$(".mento .prev").on("click", function(e){
+    e.preventDefault();
+
+    if(enableClick){
+        enableClick = false;
+        
+        clearInterval(timer);
+        movingLeft(".mento .slider-wrapper", ".slider", -50);
+    }
+});
+
+$(".mento .next").on("click", function(e){
+    e.preventDefault();
+
+    if(enableClick){
+        enableClick = false;
+        
+        clearInterval(timer);
+        movingRight(".mento .slider-wrapper", ".slider", -100, -50)
+    }
+});
+
+//box3 slide
+$(".box3 .slider-wrapper .slider").last().prependTo(".box3 .slider-wrapper");
+
+let timer2 = setInterval(function(){
+    movingRight(".box3 .slider-wrapper", ".slider", -100, 0);
+}, speed*3);
+
+$(".box3 .frame .prev").on("click", function(e){
+    e.preventDefault();
+
+    if(enableClick){
+        enableClick = false;
+        
+        clearInterval(timer2);
+        movingLeft(".box3 .slider-wrapper", ".slider", -100);
+    }
+});
+
+$(".box3 .frame .next").on("click", function(e){
+    e.preventDefault();
+
+    if(enableClick){
+        enableClick = false;
+        
+        clearInterval(timer2);
+        movingRight(".box3 .slider-wrapper", ".slider", -200, -100);
+    }
+});
+
+
+
+function movingLeft(frame, slider, def_percent){
+    $(frame).animate({marginLeft: 0}, speed/2, function(){
+        $(this).css({marginLeft: def_percent +"%"});
+        $(frame).children(slider).last().prependTo(frame);
+        enableClick = true;
+    })
+}
+
+function movingRight(frame, slider, percent, def_percent){
+    $(frame).animate({marginLeft: percent +"%"}, speed/2, function(){
+        $(this).css({marginLeft: def_percent +"%"});
+        $(frame).children(slider).first().appendTo(frame);
+        enableClick = true;
+    })
+}
