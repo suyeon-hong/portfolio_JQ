42b1716b54ff11c2b4ec7137ef0f750473e1b624
suyeon-hong <bellahong95@gmail.com>
2021-11-08 16:05:02 +0900

    메인 visual letterMotion 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
+// visual letter motion
+const letter1 = $("#visual .inner >h1");
+const letter2 = $("#visual .inner >h2");
+
+letterMotion(letter1, 0.1);
+letterMotion(letter2, 0.2);
+
+
+function letterMotion(item, delay){
+    let txt = item.text().split("");
+    let bg = $(item).css("color");
+    let num = 0;
+    $(item).empty();
+
+    $(txt).each(function(_, data){
+        $(item).append(
+            $("<span>").text(data).css({transitionDelay: delay * num +"s"})
+        )
+        num++;
+    });
+    
+    $(item).append(
+        $("<p>").css({
+            position: "absolute",
+            background: bg,
+            width: "100%",
+            height: "100%",
+            top: 0,
+            left: 0
+        })
+    );
+
+    item.find("p").animate({left: "100%"}, speed, function(){
+        $(this).remove();
+    });
+    item.find("span").css({opacity: 1});
+}
+
+
 // visual tab button
 $("#visual .filter li a").on("click", function(e){
     e.preventDefault();
@@ -7,5 +46,4 @@ $("#visual .filter li a").on("click", function(e){
     $(this).addClass("on");
     $("#visual .wrapbox >.wrap").slideUp(speed/2);
     $("#visual .wrapbox >.wrap").eq(target).slideDown(speed);
-
 })
