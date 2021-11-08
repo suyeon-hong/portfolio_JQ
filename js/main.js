6414203b87f2bc0881a39438977966d665483d7e
suyeon-hong <bellahong95@gmail.com>
2021-11-08 15:12:02 +0900

    메인 비주얼 수정

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 // visual tab button
 $("#visual .filter li a").on("click", function(e){
     e.preventDefault();
+    let target = $(this).parent().index() - 1;
 
     $("#visual .filter li a").removeClass("on");
     $(this).addClass("on");
+    $("#visual .wrapbox >.wrap").slideUp(speed/2);
+    $("#visual .wrapbox >.wrap").eq(target).slideDown(speed);
+
 })
