2bbcd339055d21b7299f210d4816142699be846b
bellahong95 <bellahong95@gmail.com>
2021-11-20 16:34:02 +0900

    flickr es5 객체지향 함수로 수정

--- js/myFlickr.js
+++ js/myFlickr.js
@@ -4,115 +4,115 @@ https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
 
 21be590b77fb11bd12a7266f99a2f2d8
 */
-const gallery = $(".gallery");
-const tabBox = gallery.find(".tab");
-const photoBox = gallery.find(".photos");
-const loadingImg = gallery.find(".loading");
-let totalImg = 11;
-let tag = "landscape";
+function MyFlickr(opt){
+    this.init(opt);
+    this.bindingEvent();
+}
 
-getList({
-    type: "search",
-    tag: tag,
-    user_id: "194091829@N05"
-});
+MyFlickr.prototype.init = function(opt){
+    this.gallery = $(opt.selector);
+    this.tabBox = $(opt.tabBox);
+    this.photoBox = $(opt. photoBox);
+    this.loadingImg = $(opt. loadingImg);
+    this.api_key = opt.api_key,
+    this.user_id = opt.user_id;
+    this.type = opt.type;
+    this.tag = opt.tag;
+    this.totalImg = opt.totalImg;
+}
 
-gallery.find("button").on("click", function(){
-    tag = gallery.find("input").val();
-
-    if(!tag){
-        alert("검색어를 입력해 주세요.");
-        return;
-    }
-    loadingImg.removeClass("off");
-    photoBox.removeClass("on");
-
-    getList({
-        type: "search",
-        tag: tag
-    });
-});
-
-$(window).on("keypress", function(e){
-    if(e.key == "Enter"){
-        tag = gallery.find("input").val();
-
-        if(!tag){
+MyFlickr.prototype.bindingEvent = function(){
+    this.getList();
+    
+    this.gallery.find("button").on("click", function(){
+        this.tag = this.gallery.find("input").val();
+    
+        if(!this.tag){
             alert("검색어를 입력해 주세요.");
             return;
         }
-        loadingImg.removeClass("off");
-        photoBox.removeClass("on");
-
-        getList({
-            type: "search",
-            tag: tag
-        });
-    }
-});
-
-$("body").on("click", ".photos li", function(e){
-    e.preventDefault();
-
-    let imgSrc = $(this).find("a").attr("href");
-
-    $(".pop").remove();
-    $("body").append(
-        $("<div class='pop'>").append(
-            $("<img>").attr({src: imgSrc}),
-            $("<span>").text("CLOSE")
+        this.loadingImg.removeClass("off");
+        this.photoBox.removeClass("on");
+        this.user_id = undefined;
+    
+        this.getList();
+    }.bind(this));
+    
+    $(window).on("keypress", function(e){
+        if(e.key == "Enter"){
+            this.tag = this.gallery.find("input").val();
+    
+            if(!this.tag){
+                alert("검색어를 입력해 주세요.");
+                return;
+            }
+            this.loadingImg.removeClass("off");
+            this.photoBox.removeClass("on");
+            this.user_id = undefined;
+    
+            this.getList();
+        }
+    }.bind(this));
+    
+    $("body").on("click", this.photoBox.selctor +(" li"), function(e){
+        e.preventDefault();
+    
+        let imgSrc = $(e.currentTarget).find("a").attr("href");
+    
+        $(".pop").remove();
+        $("body").append(
+            $("<div class='pop'>").append(
+                $("<img>").attr({src: imgSrc}),
+                $("<span>").text("CLOSE")
+            )
         )
-    )
-});
-
-$("body").on("click", ".pop span", function(){
-    $(".pop").remove();
-});
-
-tabBox.find("li a").on("click", function(e){
-    e.preventDefault();
-    tag = $(this).text();
-    loadingImg.removeClass("off");
-    photoBox.removeClass("on");
-    tabBox.find("li a").removeClass("on");
-    $(this).addClass("on");
-
-    getList({
-        type: "search",
-        tag: tag,
-        user_id: "194091829@N05"
     });
-});
+    
+    $("body").on("click", ".pop span", function(){
+        $(".pop").remove();
+    });
+    
+    this.tabBox.find("li a").on("click", function(e){
+        e.preventDefault();
+        this.tag = $(e.currentTarget).text();
+        this.loadingImg.removeClass("off");
+        this.photoBox.removeClass("on");
+        this.tabBox.find("li a").removeClass("on");
+        $(e.currentTarget).addClass("on");
+    
+        this.getList();
+    }.bind(this));
+}
 
-function getList(opt){
+MyFlickr.prototype.getList = function(){
     let result_opt = {};
 
-    if(opt.type == "interest"){
+    if(this.type == "interest"){
         result_opt = {
             url: "https://www.flickr.com/services/rest/?method=flickr.interestingness.getList",
             datatype: "json",
             data: {
-                api_key: "21be590b77fb11bd12a7266f99a2f2d8",
-                per_page: totalImg,
+                api_key: this.api_key,
+                per_page: this.totalImg,
                 format: "json",
                 nojsoncallback: 1,
                 privacy_filter: 1,
             }
         }
     }
-    if(opt.type == "search"){
-        if(opt.user_id == undefined) opt.user_id = ""
+    if(this.type == "search"){
+        if(this.user_id == undefined) this.user_id = ""
         result_opt = {
             url: "https://www.flickr.com/services/rest/?method=flickr.photos.search",
             datatype: "json",
             data: {
-                api_key: "21be590b77fb11bd12a7266f99a2f2d8",
-                per_page: totalImg,
+                api_key: this.api_key,
+                per_page: this.totalImg,
                 format: "json",
                 nojsoncallback: 1,
                 privacy_filter: 1,
-                tags: opt.tag,
-                user_id: opt.user_id
+                tags: this.tag,
+                user_id: this.user_id
             }
         }
     }
@@ -121,15 +121,15 @@ function getList(opt){
     .success(function(data){
         let items = data.photos.photo;
         
-        photoBox.empty();
-        $(items).each(function(index,data){
+        $(this.photoBox).empty();
+        $(items).each(function(_,data){
             let title = data.title;
     
             if(!data.title){
                 title = "No description in this photo"
             }
 
-            photoBox.append(
+            $(this.photoBox).append(
                 $("<li class='item'>").append(
                     $("<a>").attr({
                         href: "https://live.staticflickr.com/"+ data.server +"/"+ data.id +"_"+ data.secret +"_b.jpg",
@@ -141,30 +141,30 @@ function getList(opt){
                     ),
                     $("<h2>").text(title),
                     $("<h3>").text("CATEGORY |").append(
-                        $("<span>").text(tag),
+                        $("<span>").text(this.tag),
                         $("<span class='id'>").text(data.owner)
                     )
                 )
             )
-        });
+        }.bind(this));
     
-        loadImg();
-    }).error(function(err){
+        this.loadImg();
+    }.bind(this)).error(function(err){
         console.error("데이터를 불러오지 못했습니다.");
-    })
+    });
 }
 
-function loadImg(){
+MyFlickr.prototype.loadImg = function(){
     let imgNum = 0;
 
-    photoBox.find("img").each(function(_, data){
+    this.photoBox.find("img").each(function(_, data){
         data.onload = function(){
             imgNum++;
 
-            if (imgNum == totalImg){
-                loadingImg.addClass("off");
+            if (imgNum == this.totalImg){
+                this.loadingImg.addClass("off");
             }
-            photoBox.addClass("on");
-        }
-    });
+            this.photoBox.addClass("on");
+        }.bind(this);
+    }.bind(this));
 }
