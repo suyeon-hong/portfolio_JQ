3a86e45f244625ec050e50fb61985103de914a25
bellahong95 <bellahong95@gmail.com>
2021-11-20 17:20:27 +0900

    youtube 함수 정리

--- js/youtube.js
+++ js/youtube.js
@@ -1,61 +1,79 @@
-$.ajax({
-    url:"https://www.googleapis.com/youtube/v3/playlistItems",
-    dataType: "jsonp",
-    data: {
-        part: "snippet",
-        key: "AIzaSyDLlgfKVBsGJubow2HpPxjC1LJgGRtpBHA",
-        maxResults: 10,
-        playlistId: "PLbO44G2j_RJzPLODoK6qiJwJ39JK4JyCY"
-    }
-}).success(function(data){
-    let items = data.items;
 
-    $(items).each(function(index, data){
-        let txt = data.snippet.description;
-        let len = txt.length;
 
-        if (len > 200){
-            txt = txt.substr(0, 200)+ "..";
-        }
+init();
+bindingEvent();
 
-        $("#vidGallery").append(
-            $("<article>").append(
-                $("<a>").attr({
-                    href : data.snippet.resourceId.videoId
-                }).append(
-                    $("<img>").attr({src: data.snippet.thumbnails.high.url})
-                ),
-                $("<h3>").text(data.snippet.title),
-                $("<p>").text(txt),
-                $("<span class='play'>").text("비디오재생").append(
-                    $("<i class='fas fa-play'>")
-                )
+function init(){
+    const frame = $("#vidGallery");
+    const key = "AIzaSyDLlgfKVBsGJubow2HpPxjC1LJgGRtpBHA";
+    const playlist = "PLbO44G2j_RJzPLODoK6qiJwJ39JK4JyCY";
+    const num = 10;
+}
+
+function bindingEvent(){
+    createVid();
+
+    $("body").on("click", frame.selector +(" article .play"), function(e){
+        e.preventDefault();
+    
+        let vidId = $(e.currentTarget).parent().find("a").attr("href");
+    
+        $("body").append(
+            $("<div class='pop'>").append(
+                $("<iframe>").attr({
+                    src: "https://www.youtube.com/embed/"+ vidId,
+                    width: "100%",
+                    height: "100%",
+                    frameborder: 0,
+                    allowfullscreen: true,
+                }),
+                $("<span class='close'>").text("CLOSE")
             )
         )
     });
-}).error(function(err){
-    console.error(err);
-});
+    
+    $("body").on("click", ".close", function(){
+        $(".pop").remove();
+    })
+}
 
-$("body").on("click", "#vidGallery article .play", function(e){
-    e.preventDefault();
-
-    let vidId = $(this).parent().find("a").attr("href");
-
-    $("body").append(
-        $("<div class='pop'>").append(
-            $("<iframe>").attr({
-                src: "https://www.youtube.com/embed/"+ vidId,
-                width: "100%",
-                height: "100%",
-                frameborder: 0,
-                allowfullscreen: true,
-            }),
-            $("<span>").text("CLOSE")
-        )
-    )
-});
-
-$("body").on("click", ".pop", function(){
-    $(".pop").remove();
-})
+function createVid(){
+    $.ajax({
+        url:"https://www.googleapis.com/youtube/v3/playlistItems",
+        dataType: "jsonp",
+        data: {
+            part: "snippet",
+            key: key,
+            maxResults: num,
+            playlistId: playlist
+        }
+    }).success(function(data){
+        let items = data.items;
+    
+        $(items).each(function(_, data){
+            let txt = data.snippet.description;
+            let len = txt.length;
+    
+            if (len > 200){
+                txt = txt.substr(0, 200)+ "..";
+            }
+    
+            $(frame).append(
+                $("<article>").append(
+                    $("<a>").attr({
+                        href : data.snippet.resourceId.videoId
+                    }).append(
+                        $("<img>").attr({src: data.snippet.thumbnails.high.url})
+                    ),
+                    $("<h3>").text(data.snippet.title),
+                    $("<p>").text(txt),
+                    $("<span class='play'>").text("비디오재생").append(
+                        $("<i class='fas fa-play'>")
+                    )
+                )
+            )
+        });
+    }).error(function(err){
+        console.error(err);
+    });
+}
