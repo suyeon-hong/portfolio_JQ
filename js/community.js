363bfd6fc8ac2d363617ce538825a8b4350cb33d
suyeon-hong <bellahong95@gmail.com>
2021-12-21 12:09:57 +0900

    community 게시글 json 파일로 불러오기

--- js/community.js
+++ js/community.js
@@ -1,6 +1,8 @@
-const $tabs = $(".community .tabs li a");
-const $faqs = $(".community #faq .numbers span");
-const $notices = $(".community #notice .numbers span");
+const $tabs = $(".tabs li a");
+const $faqs = $("#faq .numbers span");
+const $notices = $("#notice .numbers span");
+const $qna = $("#faq .qna");
+const $notice = $("#notice tbody");
 
 activation($tabs);
 activation($faqs);
@@ -12,5 +14,47 @@ function activation(item){
 
         item.removeClass("on");
         $(this).addClass("on");
+    });
+}
+
+$.ajax({
+    url: "js/data.json",
+    datatype: "json"
+}).success(items=> {
+    const itemsQna = items.qna;
+    const itemsNotice = items.notice;
+
+    createList(itemsQna);
+    createList2(itemsNotice);
+})
+
+function createList(items){
+    $(items).each((_,item)=>{
+        $qna.append(
+            $("<dt>").append(
+                $("<span class='icon'>").text("Q"),
+                $("<p>").text(item.question),
+                $("<span class='subj'>").text(item.subject),
+                $("<a href='#' class='arrow' title='답변보기'>")
+            ),
+            $("<dd>").append(
+                $("<span class='icon'>").text("A"),
+                $("<p>").text(item.answer)
+            )
+        )
+    });
+}
+
+function createList2(items){
+    $(items).each((index,item)=>{
+        $notice.prepend(
+            $("<tr>").append(
+                $("<td>").text(index + 1),
+                $("<td>").append(
+                    $("<a href='#'>").text(item.title)
+                ),
+                $("<td>").text(item.date)
+            )
+        )
     });
 }
